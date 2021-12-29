dfc957a373fca30ae374448ef56afd85bcc59739
dyddlfsla <7942dyddlfsl@naver.com>
2021-12-30 02:27:49 +0900

    Initial commit

---
+++ src/main/java/com/example/pamphboard/PamphboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.pamphboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamphboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamphboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pamphboard/PamphboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pamphboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamphboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
