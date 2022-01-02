6ae98fdaf1d799b5bc6212f57b5c8a4f6881746b
dyddlfsla <7942dyddlfsl@naver.com>
2022-01-03 03:23:45 +0900

    feat: HomeController 추가 및 테스트 작성

---
+++ src/test/java/com/example/pamphboard/web/HomeControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.pamphboard.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class HomeControllerTest {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Test
+  public void homeTest() {
+    //when
+    String body = this.restTemplate.getForObject("/", String.class);
+
+    //then
+    assertThat(body).contains("PamphBoard");//URL 로 호출되는 페이지에 해당 문자열 정보가 담겨 있는지 확인
+  }
+
+
+}
