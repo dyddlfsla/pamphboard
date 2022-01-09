3324e80a73fc2772ecb3f52b3bdb55fdb1363417
dyddlfsla <7942dyddlfsl@naver.com>
2022-01-10 02:47:37 +0900

    feat: PostMapper.java 추가

---
+++ src/main/java/com/example/pamphboard/mapper/PostMapper.java
@@ -0,0 +1,14 @@
+package com.example.pamphboard.mapper;
+
+import com.example.pamphboard.dto.PostDto;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PostMapper {
+
+  @Insert("insert into BOARD_POST(writer, password, title, content) "
+        + "values(#{writer}, #{password}, #{title}, #{content})")
+  void savePosts(PostDto postDto);
+
+}
