005094d1713c09543e86b0162175d7d9653d4015
dyddlfsla <7942dyddlfsl@naver.com>
2022-01-10 02:48:56 +0900

    feat: PostService.java 추가

---
+++ src/main/java/com/example/pamphboard/service/PostService.java
@@ -0,0 +1,21 @@
+package com.example.pamphboard.service;
+
+import com.example.pamphboard.dao.PostDao;
+import com.example.pamphboard.dto.PostDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+  private PostDao postDao;
+
+  @Autowired
+  PostService(PostDao postDao) {
+    this.postDao = postDao;
+  }
+
+  public void savePosts(PostDto postDto) {
+    postDao.savePosts(postDto);
+  }
+}
