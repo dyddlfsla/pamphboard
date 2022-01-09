8a0e82a8030bd0a6e536aa156af2275e5255effd
dyddlfsla <7942dyddlfsl@naver.com>
2022-01-10 02:06:18 +0900

    refactor: 메서드명 위치 수정

--- src/main/java/com/example/pamphboard/web/HomeController.java
+++ src/main/java/com/example/pamphboard/web/HomeController.java
@@ -10,10 +10,4 @@ public class HomeController {
   public String home() {
     return "home";
   }
-
-  @GetMapping("/posts/save")
-  public String saveArticles() {
-    return "posts-save";
-  }
-
 }
