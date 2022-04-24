063ae1d6719ab98a6054ec7e71601da573cd970e
dyddlfsla <7942dyddlfsl@naver.com>
2022-04-25 01:58:45 +0900

    fix: mapper 쿼리 맵핑 수정

--- src/main/java/com/example/pamphboard/mapper/PostMapper.java
+++ src/main/java/com/example/pamphboard/mapper/PostMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface PostMapper {
@@ -14,13 +15,13 @@ public interface PostMapper {
         + "VALUES(#{author}, #{password}, #{title}, #{content})")
   Long insert(PostDto postDto);
 
-  @Select("SELECT * FROM BOARD_POST WHERE post_idx = #{post_idx}")
+  @Select("SELECT * FROM BOARD_POST WHERE post_idx = #{postIdx}")
   PostDto selectById(long postIdx);
 
   @Select("SELECT * FROM BOARD_POST")
   List<PostDto> selectAll();
 
-  @Delete("DELETE FROM BOARD_POST WHERE post_idx = #{post_idx}")
+  @Delete("DELETE FROM BOARD_POST WHERE post_idx = #{postIdx}")
   Long deleteById(long postIdx);
 
   @Update("UPDATE BOARD_POST SET title = #{title}, content = #{content} WHERE post_idx = #{postIdx}")
