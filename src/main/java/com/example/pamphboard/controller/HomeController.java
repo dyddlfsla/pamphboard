133ae113b21f7b8c6995218657a8205067317b43
dyddlfsla <7942dyddlfsl@naver.com>
2022-04-25 21:15:27 +0900

    chore: redirect 추가

--- src/main/java/com/example/pamphboard/controller/HomeController.java
+++ src/main/java/com/example/pamphboard/controller/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
   @GetMapping("/")
+  public String redirect() {
+    return "redirect:/home";
+  }
+
+  @GetMapping("/home")
   public String home() {
     return "home";
   }
